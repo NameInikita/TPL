f5fe7b5de3faac18779a9405b318cfb7a1e122d9
NameInikita <name.inikita@gmail.com>
2017-08-03 23:17:25 +0300

    TPL, синхронный/асинхронный метода

---
+++ TPL_Action/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPL_Action
+{
+    class Program
+    {
+        //Метод, который планируется запустить асинхронно
+
+        static void MyTask()
+        {
+            int threadID = Thread.CurrentThread.ManagedThreadId;
+
+            Console.Write("MyTask запущен в потоке {0}, CurrentId {1}", threadID,Task.CurrentId);
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(j+ " ");
+                    Thread.Sleep(100);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("MyTask завершился в потоке {0}, CurrentId {1}", threadID,Task.CurrentId);
+
+        }
+
+        static void Main(string[] args)
+        {
+            int threadID = Thread.CurrentThread.ManagedThreadId;
+
+            Console.WriteLine("Main запущен в потоке {0}", threadID);
+
+            Action action = new Action(MyTask);
+
+            Task taskAction = new Task(action);//Создание  экземпляра задачи через делегат Action 
+            //taskAction.Start();              //Запуск задачи на выполнение асинхронно.
+            taskAction.RunSynchronously();    //Запуск задачи на выполнение синхронно.
+            taskAction.Wait();                // Ожидание завершения асинхронной задачи.
+            Task taskAction2 = new Task(action);//для проверки  CurrentId
+            //taskAction2.Start();
+            taskAction2.RunSynchronously();
+
+
+
+            //Task task = new Task(MyTask);//Создание  экземпляра задачи на классе Task
+            //task.Start();                //Запуск задачи на выполнение асинхронно.
+            ////task.RunSynchronously();   //Запуск задачи на выполнение синхронно.
+
+            Console.ReadKey();
+        }
+    }
+}
